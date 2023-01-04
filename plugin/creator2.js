e973c41bb5baa4656007a72f56d6f59dceddf493
Profile <91337596+SAKUTA123@users.noreply.github.com>
2023-01-04 16:54:12 +0800

    Update and rename plugins/creator2.js to plugin/creator2.js

--- plugin/creator2.js
+++ plugin/creator2.js
@@ -5,7 +5,7 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
 if (command == 'owner') {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${wm};;;\nFN:${wm}\nORG:${wm}\nTITLE:\nitem1.TEL;waid=${nomorown}:${nomorownv2}\nitem1.X-ABLabel:AbiDev\nX-WA-BIZ-DESCRIPTION:Ada Yang Bisa Saya Bantu?\nX-WA-BIZ-NAME:${wm}\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${wm};;;\nFN:${wm}\nORG:${wm}\nTITLE:\nitem1.TEL;waid=${nomorown}:${nomorownv2}\nitem1.X-ABLabel:Leon\nX-WA-BIZ-DESCRIPTION:Ada Yang Bisa Saya Bantu?\nX-WA-BIZ-NAME:${wm}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }},)
   }
   }
